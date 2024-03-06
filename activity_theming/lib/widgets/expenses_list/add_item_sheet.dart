3670fbd9c3944b77d564074502aafcc8f7120082
Shinhyeonjung <tlswnd3127@gmail.com>
2024-03-06 17:04:56 +0900

    Feat Input, Date Input

--- activity_theming/lib/widgets/expenses_list/add_item_sheet.dart
+++ activity_theming/lib/widgets/expenses_list/add_item_sheet.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AddItemSheet extends StatefulWidget {
   const AddItemSheet({super.key});
@@ -17,9 +19,23 @@ class _AddItemSheet extends State<AddItemSheet> {
   // }
 
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  void _presentDatePicker() {
+    final nowTime = DateTime.now();
+    final firstDate = DateTime(nowTime.year - 1, nowTime.month, nowTime.day);
+
+    showDatePicker(
+        context: context,
+        initialDate: nowTime, //
+        firstDate: firstDate,
+        lastDate: nowTime);
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -34,17 +50,52 @@ class _AddItemSheet extends State<AddItemSheet> {
             controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.text,
-            decoration: InputDecoration(label: Text('Title')),
+            decoration: InputDecoration(label: Text('제목')),
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  // onChanged: _saveTitleInput,
+                  controller: _amountController,
+                  maxLength: 50,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      prefixText: '\$ ', // 앞에 해당 문자열 추가 추가
+                      label: Text('사용금액')),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('Select Date'),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: Icon(Icons.date_range))
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('취소'),
+              ),
               ElevatedButton(
                   onPressed: () {
-                    print(_titleController.text);
+                    print(_amountController.text);
                   },
                   child: const Text('SAVE')),
             ],
-          )
+          ),
         ],
       ),
     );
